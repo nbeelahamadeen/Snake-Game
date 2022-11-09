0c5cf3d3904d880e75a0ed748b328abd22d26f6d
nbeelahamadeen <nbeelahamadeen@gmail.com>
2022-11-09 11:08:03 -0600

    added movement to snake body!

--- arcade/app.js
+++ arcade/app.js
@@ -2,8 +2,9 @@ let table = document.querySelector('#snakeBoard');
 let scoreDisplay = document.querySelector('#scoreDisplay');
 
 //SNAKE components
-let snake = [[10 , 10], [10 , 11]];
-let nextDirection = [0 , 1];
+let snake = [[10 , 10]];
+let nextDirection = [1 , 0];
+let head = 0;
 
 
 
@@ -22,6 +23,7 @@ drawSnake();
 randomApple();
 
 
+
 //draws a snake on the snakeboard
 function drawSnake(){
     for(let i = 0; i < snake.length; i++){
@@ -35,43 +37,58 @@ function drawSnake(){
 
 
 
-
+//moves snake and removes the tail
 function moveSnake(){    
 for(let i = 0; i < snake.length;i++){
-         let tail = snake[0];
-         let x = tail[0];
-         let y = tail[1];
-         let postion = x + "-" + y;
+         let body = snake[i];
+         console.log(body)
+         let y = body[0];
+         let x = body[1];
+         let postion = y + "-" + x;
          document.getElementById(postion).classList.remove('snake')
-         snake[i][1]++;       
-         console.log(tail)         
+         let newY = nextDirection[0]; 
+         let newX = nextDirection[1]; 
+         body[0] = body[0] + newY;
+         body[1] = body[1] + newX;
+        
         }
     };
 
-changeDirection();
+    window.addEventListener('keydown', control);
+      function control(event){
+        //move up/W
+        if(event.keyCode === 38 || event.keyCode === 87)
+        {           
+        nextDirection = [-1,0];
+        //move down/S
+        } else if(event.keyCode === 40 || event.keyCode === 83){
+            nextDirection = [1,0];
+        //move left/A 
+        } else if(event.keyCode === 37 || event.keyCode === 65){
+            nextDirection = [0,-1];
+        //move right/D
+        } else if(event.keyCode === 39 || event.keyCode === 68){
+            nextDirection = [0,1];
+        }
+      }  
 
-function changeDirection(){
-    for(let i = 0; i < snake.length;i++){
-    let head = snake[snake.length-1];
-    let headX = headX + snake[i][0];
-    let headY = headY + snake[i][1];
-    console.log(headX, headY)
-    }
-    //let direction = headX 
-    
-}
+//     changeDirection();
 
-    // document.addEventListener('keydown', control);
-    //   function control(ev){
-    //     if(ev.keycode == 38 || ev.keycode == 87){
-
-    //     }
-    //   }  
+// function changeDirection(){
+//     for(let i = 0; i < snake.length;i++){
+//     let snakePostition = snake[i];
+//        let x = snakePostition[0];
+//        let y = snakePostition[1];
+//        let head = x + "-" + y;
+//        console.log(head)
+//     }   
+// }
 
 setInterval(function(){
          moveSnake();
          drawSnake();
-    },1000);
+         //changeDirection();
+    },300);
 
 
 
@@ -81,7 +98,7 @@ function randomApple(){
     let appleX = Math.ceil(Math.random() * 20);
     let appleY = Math.ceil(Math.random() * 20);
     let postion = appleX + "-" + appleY;
-    if(drawSnake !== postion){
+    if(snake !== postion){
     document.getElementById(postion).classList.add('apple');
     }
 }
