927918f0c9013314c8745b5da6f97ce7e266f2a0
nbeelahamadeen <nbeelahamadeen@gmail.com>
2022-11-11 16:32:11 -0600

    revampped the snakemovement function, created remove snake funciton, created funciton to look into snake body, create edge case for hitting walls and hitting itself

--- arcade/app.js
+++ arcade/app.js
@@ -4,8 +4,17 @@ let score = document.querySelector('#scoreDisplay');
 //SNAKE components
 let snake = [[10 , 10]];
 let nextDirection = [1 , 0];
-let applePosition = 0;
 let points = 0;
+let applePosition = 0;
+let lastPostion = [0, 0];
+
+//refershes the screen and creates the moving
+let interval = setInterval(function(){
+    removeSnake();
+    moveSnake();
+    drawSnake();
+    eatApple();
+},250);
 
 
 //creation of cellID
@@ -20,7 +29,7 @@ for(let x = 0; x < 21; x++){
     }
 }
 
-drawSnake();
+// create apple in random spot
 randomApple();
 
 
@@ -36,23 +45,50 @@ function drawSnake(){
     }
 }
 
+//This function loops through the snake
+function moveSnake(){
+    let head = snake[snake.length -1];
+    let nextHead = [head[0] + nextDirection[0] , head[1] + nextDirection[1]];
+    console.log(nextHead);
+    if(nextHead[0] < 0 || nextHead[0] >= 21){
+        score.innerText = 'GAME OVER! Your score was ' + points;
+        clearInterval(interval);
+    }else if(nextHead[1] < 0 || nextHead[1] >= 21){
+        score.innerText = 'GAME OVER! Your score was ' + points;
+        clearInterval(interval);
+    }else if(partOfSnake(nextHead)){
+        score.innerText = 'GAME OVER! Your score was ' + points;
+        clearInterval(interval);
+    }
+    snake.push(nextHead);
+    lastPostion = snake.shift(snake);
+    console.log(lastPostion);
+}
 
+function removeSnake(){
+    for(let i = 0; i < snake.length;i++){
+        let body = snake[i];
+        let y = body[0];
+        let x = body[1];
+        let postion = y + "-" + x;
+        document.getElementById(postion).classList.remove('snake')
+    }
+}
 
-//moves snake and removes the tail
-function moveSnake(){    
-for(let i = 0; i < snake.length;i++){
-         let body = snake[i];
-         let y = body[0];
-         let x = body[1];
-         let postion = y + "-" + x;
-         document.getElementById(postion).classList.remove('snake')
-         let newY = nextDirection[0]; 
-         let newX = nextDirection[1]; 
-         body[0] = body[0] + newY;
-         body[1] = body[1] + newX;
-        
+function growBody(){
+    snake.unshift(lastPostion);
+}
+
+function partOfSnake(applePosition){
+    for(let i = 0; i < snake.length;i++){
+        let body = snake[i];
+        if(applePosition[0] == body[0] && applePosition[1] == body[1]){
+            return true;
         }
-    };
+    }   
+    return false;
+}
+
 
     window.addEventListener('keydown', control);
       function control(event){
@@ -73,21 +109,17 @@ for(let i = 0; i < snake.length;i++){
       }  
 
 
-
-
-setInterval(function(){
-         moveSnake();
-         drawSnake();
-         eatApple();
-    },500);
-
-
-
 //Apple component
 //produce random apple on the snakeboard where there isnt a snake
 function randomApple(){
     let appleX = Math.ceil(Math.random() * 20);
     let appleY = Math.ceil(Math.random() * 20);
+    
+    while(partOfSnake([appleX, appleY])){
+        appleX = Math.ceil(Math.random() * 20);
+        appleY = Math.ceil(Math.random() * 20);
+        
+    }
     applePosition = appleX + "-" + appleY;
     document.getElementById(applePosition).classList.add('apple');
 
@@ -99,13 +131,12 @@ function eatApple(){
        let x = snakePostition[0];
        let y = snakePostition[1];
        let head = x + "-" + y;
-       console.log(head);
-       console.log(applePosition);
        if(head === applePosition){
         document.getElementById(head).classList.remove('apple');
         points++;
         score.innerText = "score "+ points;
-        randomApple()
+        growBody();
+        randomApple();        
         }
     }   
 }
