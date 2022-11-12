06daaeff0e71abd0fdab134a0a508441776ad530
nbeelahamadeen <nbeelahamadeen@gmail.com>
2022-11-12 16:50:05 -0600

    updated js file with comments and purpose of functions

--- arcade/app.js
+++ arcade/app.js
@@ -1,23 +1,33 @@
+//DOM components
 let table = document.querySelector('#snakeBoard');
 let score = document.querySelector('#scoreDisplay');
 
-//SNAKE components
+//SNAKE components and global varaibles
 let snake = [[10 , 10]];
 let nextDirection = [1 , 0];
 let points = 0;
 let applePosition = 0;
 let lastPostion = [0, 0];
+let interval = 0;
 
-//refershes the screen and creates the moving
-let interval = setInterval(function(){
-    removeSnake();
-    moveSnake();
-    drawSnake();
-    eatApple();
-},250);
+//Game starts once a button is pressed
+window.addEventListener('keydown', startGame);
+
+//This function moves the snake, creates the moving animation as well as updates the score
+function startGame(){
+
+        clearInterval(interval);
+        interval = setInterval(function(){
+        removeSnake();
+        moveSnake();
+        drawSnake();
+        eatApple();
+    },250);
+}
 
 
-//creation of cellID
+
+//creation of cellID and grid
 for(let x = 0; x < 21; x++){
     let row = document.createElement('tr');
     table.appendChild(row);
@@ -49,7 +59,8 @@ function drawSnake(){
 function moveSnake(){
     let head = snake[snake.length -1];
     let nextHead = [head[0] + nextDirection[0] , head[1] + nextDirection[1]];
-    console.log(nextHead);
+
+    //checks if the next head is the wall and stops the game
     if(nextHead[0] < 0 || nextHead[0] >= 21){
         score.innerText = 'GAME OVER! Your score was ' + points;
         clearInterval(interval);
@@ -60,11 +71,12 @@ function moveSnake(){
         score.innerText = 'GAME OVER! Your score was ' + points;
         clearInterval(interval);
     }
+    //if the next head is not the wall it will add a new snake head
     snake.push(nextHead);
     lastPostion = snake.shift(snake);
-    console.log(lastPostion);
 }
 
+//removes the snake's class fromt the last snake location 
 function removeSnake(){
     for(let i = 0; i < snake.length;i++){
         let body = snake[i];
@@ -75,10 +87,12 @@ function removeSnake(){
     }
 }
 
+//adds the last snake postion to the tail of the snake body
 function growBody(){
     snake.unshift(lastPostion);
 }
 
+//This function continously loops through the snake and checks for the apple position
 function partOfSnake(applePosition){
     for(let i = 0; i < snake.length;i++){
         let body = snake[i];
@@ -89,7 +103,7 @@ function partOfSnake(applePosition){
     return false;
 }
 
-
+    //changes the direction once the arrow or movement keys are pressed
     window.addEventListener('keydown', control);
       function control(event){
         //move up/W
@@ -109,12 +123,14 @@ function partOfSnake(applePosition){
       }  
 
 
-//Apple component
+//Apple components
+
 //produce random apple on the snakeboard where there isnt a snake
 function randomApple(){
     let appleX = Math.ceil(Math.random() * 20);
     let appleY = Math.ceil(Math.random() * 20);
     
+    //checks if the apple is on a part of the snake and gererates a new apple if it is
     while(partOfSnake([appleX, appleY])){
         appleX = Math.ceil(Math.random() * 20);
         appleY = Math.ceil(Math.random() * 20);
@@ -131,10 +147,13 @@ function eatApple(){
        let x = snakePostition[0];
        let y = snakePostition[1];
        let head = x + "-" + y;
+       //removes the apple once the head is on the apple
        if(head === applePosition){
         document.getElementById(head).classList.remove('apple');
+        //updates the points
         points++;
         score.innerText = "score "+ points;
+        //grows the body and generate new apple
         growBody();
         randomApple();        
         }
